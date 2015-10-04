49316db5e5448578390724a33dfbc40ac7713602
jl3387 <jl3387@cornell.edu>
2015-10-04 17:07:45 -0400

    add basic footer header

---
+++ php/header.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Super Seminar Scraper</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Map</a></li>
+        <li><a href="#">Trending</a></li> 
+        <li><a href="#">About</a></li> 
+      </ul>
+    </div>
+  </div>
+ </nav>
