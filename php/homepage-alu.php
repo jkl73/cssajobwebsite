124ff91fcb35ebc1f4985fc874c9e370ce466352
zt95 <TZY@dhcp-rhodes-1155.redrover.cornell.edu>
2015-10-15 22:32:09 -0400

    change homepage-alu

--- php/homepage-alu.php
+++ php/homepage-alu.php
@@ -12,16 +12,80 @@
 	 <script src="js/main.js"></script>
   	<style>
   	</style>
-
 </head>
 
 <body>
 <?php
   include("header.php");
 ?>
+<div class = "container">
 <div class="row">
-  <div class="col-xs-12 col-sm-6 col-md-8">information feeds</div>
-  <div class="col-xs-6 col-md-4">personal status</div>
+  <div class="col-xs-12 col-sm-6 col-md-8">
+  	<div class = "row">
+  		<a class = "btn btn-default" href="postjob.php">Add Post</a>
+  	</div>
+  	 <ul class="list-group">
+<?php
+	//$server = mysql_connect("localhost","root","1qaz-pl,");
+	$server = mysql_connect("cssadbinstance.ccmgeu2ghiy1.us-east-1.rds.amazonaws.com", "cssaadmin", "cssaadmin123"); 
+	if (!$server) { 
+		print "Error - Could not connect to MySQL"; 
+		exit; 
+	}
+	$db = mysql_select_db("user_student"); 
+	if (!$db) { 
+		print "Error - Could not select the user_student database"; 
+		exit; 
+	}
+	if(!isset($_COOKIE['email']))
+	{
+		header('Location: index.php');
+		exit;
+	}
+	$myemail = $_COOKIE["email"];
+
+	$query = "select company,postid,email,position,visit from post_info;";
+	$result = mysql_query($query);
+	if(!$result){
+		print "Error- Get info from post_info failed";
+		$error = mysql_error();
+		print "<p>". $error . "</p>";
+		//exit;		
+	}
+
+	while($row = mysql_fetch_array($result)){
+		$num_fields = sizeof($row);
+		reset($row); 
+		echo "<li class=\"list-group-item\">";
+		echo "<span class=\"badge\">".$row["visit"]."</span>";
+		echo '<a href="#">'.$row["company"].' information by '.$row["email"].' for position: '.$row["position"].'</a>';
+		echo "</li> "; 
+	} 
+	print "</ul>";
+?>
+  </div>
+  <div class="col-xs-6 col-md-4">
+  	<ul class="list-group">
+<?php
+	$query = 'select name,grad_year,company,position,Linkedin from employer where email = "'.$myemail.'";';
+	$result = mysql_query($query);
+	if(!$result){
+		print "Error- Get info from employer failed";
+		$error = mysql_error();
+		print "<p>". $error . "</p>";
+		exit;		
+	}
+	$row = mysql_fetch_array($result);
+		$num_fields = sizeof($row);
+		echo '<li class="list-group-item">Username:'.$row["name"].'</li>';
+		echo '<li class="list-group-item">Expected Graduation Year:'.$row["grad_year"].'</li>';
+		echo '<li class="list-group-item">Company:'.$row["company"].'</li>';
+		echo '<li class="list-group-item">Position:'.$row["position"].'</li>';
+		echo '<li class="list-group-item">Linkedin Homepage:'.$row["Linkedin"].'</li>';
+	echo "</ul>"
+?>
+  </div>
+</div>
 </div>
 
 
