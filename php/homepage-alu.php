c04d3362db03661e6fcec9c5ce391305383a6816
jl3387 <jl3387@cornell.edu>
2015-10-17 21:38:22 -0400

    fix bug

--- php/homepage-alu.php
+++ php/homepage-alu.php
@@ -7,7 +7,7 @@
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
-	 <link rel="stylesheet" href="http://localhost/cssajobwebsite/css/main.css">
+	 <link rel="stylesheet" href="../css/main.css">
 	 <link rel="stylesheet" href="../css/profile.css">
 	 <script src="js/main.js"></script>
   	<style>
