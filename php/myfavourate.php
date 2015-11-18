f868bc578d3978534aafbd5fc465e93922ec2ec2
Jia Zhang <david.jia.zhang@gmail.com>
2015-11-17 20:52:15 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit d8ae3934b94a75f09d115ce6fdbd5fc6a0605105, reversing
    changes made to 4de5d602645f1e402d755b5adefc02d48d6db824.

--- php/myfavourate.php
+++ php/myfavourate.php
@@ -68,7 +68,7 @@
 			$targetstring[strlen($targetstring) - 1] = ')';
 			//echo $targetstring;
 			$res_data = sql_get_post_by_ids($targetstring);
-			Print_Post($res_data,$myemail);
+			Print_Post($res_data);
 		}
 	}
 	else if (isset($_GET["mode"]))
@@ -86,13 +86,13 @@
 			else {
 				$targetstring[strlen($targetstring) - 1] = ')';
 				$res_data = sql_get_post_by_ids($targetstring);
-				Print_Post($res_data,$myemail);
+				Print_Post($res_data);
 			}			
 		}
 	}
 	else
 	{
-		Display_all_query($myemail);
+		Display_all_query();
   	}
 ?>
 </div>
@@ -155,7 +155,7 @@
 </body>
 </html>
 <?php
-function Display_all_query($myemail)
+function Display_all_query()
 {
 	$conn = getconn();
 	$stmt = $conn->prepare("select * from post_info order by time DESC;");
@@ -166,11 +166,5 @@ function Display_all_query($myemail)
         pdo_die($stmt);
     }
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-<<<<<<< HEAD
     Print_Post($result);
 }?>
-=======
-    Print_Post($result,$myemail);
-}
-?>
->>>>>>> origin/master
